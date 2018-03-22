7135c6e1fc8b02c08fb95331e36bd949e56212db
Przemysław Wróbel <przemek.wrobel@poczta.fm>
2018-03-22 23:05:14 +0100

    Dodano prezentacje flagi

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
-
-var url = 'https://restcountries.eu/rest/v1/name/';
+var url = 'https://restcountries.eu/rest/v2/name/';
 var countriesList = $('#countries');
 
 $('#search').click(searchCountries);
 $('country-name').keypress(searchCountries);
 
 function searchCountries() {
- 	var countryName = $('#country-name').val();
-	if(!countryName.length) countryName = 'Poland';
-	$.ajax({
-	  	url: url + countryName,
-  		method: 'GET',
-  		success: showCountriesList
-  	});
+    var countryName = $('#country-name').val();
+    if (!countryName.length) countryName = 'Poland';
+    $.ajax({
+        url: url + countryName,
+        method: 'GET',
+        success: showCountriesList
+    });
 }
 
 
-   function showCountriesList(resp) {
+function showCountriesList(resp) {
     countriesList.empty();
     resp.forEach(function(item) {
-      console.log(item);
-      $('<li>')
+        console.log(item);
+        $('<li>')
 
-        // zobacz na linijkę kodu poniżej
-        .append($('<div>').prepend('<img src="' + item.flag + '">')) 
+            // zobacz na linijkę kodu poniżej
+            .append($('<div>').prepend('<img src="' + item.flag + '">'))
 
 
-        .append($('<h2>').text(item.name))
-        .append($('<p>').text('Stolica: ' + item.capital))
-        .append($('<p>').text('Kierunkowy: ' + item.callingCodes))
-        .append($('<p>').text('Region: ' + item.region))
-        .append($('<p>').text('Język: ' + item.languages))
-        .append($('<p>').text('Powierzchnia: ' + item.area + ' sq. km'))
-        .append($('<p>').text('Zaludnienie: ' + item.population))
-        .append($('<p>').text('Waluta: ' + item.currencies))
-        .appendTo(countriesList); 
-          
+            .append($('<h2>').text(item.name))
+            .append($('<p>').text('Stolica: ' + item.capital))
+            .append($('<p>').text('Kierunkowy: ' + item.callingCodes))
+            .append($('<p>').text('Region: ' + item.region))
+            .append($('<p>').text('Język: ' + item.languages))
+            .append($('<p>').text('Powierzchnia: ' + item.area + ' sq. km'))
+            .append($('<p>').text('Zaludnienie: ' + item.population))
+            .append($('<p>').text('Waluta: ' + item.currencies))
+            .appendTo(countriesList);
+
     });
 
-  
+
 }
